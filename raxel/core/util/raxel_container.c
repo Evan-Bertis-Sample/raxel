0515a5e1931256d06da2e702b475253361939b71
evan-bertis-sample <esample21@gmail.com>
2025-02-20 09:21:25 -0600

    Got more tests to pass for arrays, lists, and strings

--- raxel/core/util/raxel_container.c
+++ raxel/core/util/raxel_container.c
@@ -4,6 +4,17 @@
 
 #include "raxel_mem.h"
 
+/*---------------------------------------------------------------------------
+  Utility functions for header lookup (using char* arithmetic)
+  ---------------------------------------------------------------------------*/
+static inline __raxel_array_header_t *raxel_get_array_header(void *array_data) {
+    return (__raxel_array_header_t *)((char *)array_data - sizeof(__raxel_array_header_t));
+}
+
+static inline __raxel_list_header_t *raxel_get_list_header(void *list_data) {
+    return (__raxel_list_header_t *)((char *)list_data - sizeof(__raxel_list_header_t));
+}
+
 /**------------------------------------------------------------------------
  *                           RAXEL ARRAY
  *------------------------------------------------------------------------**/
@@ -11,25 +22,24 @@
 void *__raxel_array_create(raxel_allocator_t *allocator, raxel_size_t size, raxel_size_t stride) {
     raxel_size_t header_size = sizeof(__raxel_array_header_t);
     raxel_size_t data_size = size * stride;
-    void *array = raxel_malloc(allocator, header_size + data_size);
-    __raxel_array_header_t *header = array;
+    void *block = raxel_malloc(allocator, header_size + data_size);
+    __raxel_array_header_t *header = (__raxel_array_header_t *)block;
     header->__size = size;
     header->__stride = stride;
     header->__allocator = allocator;
-    // return the pointer to the data
-    return (void *)((raxel_size_t *)array + header_size);
+    // Return pointer to data (right after header)
+    return (void *)((char *)block + header_size);
 }
 
 void __raxel_array_destroy(void *array) {
-    __raxel_array_header_t *header = (__raxel_array_header_t *)((raxel_size_t *)array - sizeof(__raxel_array_header_t));
-    raxel_free(header->__allocator, (void *)((raxel_size_t *)array - sizeof(__raxel_array_header_t)));
+    if (!array) return;
+    __raxel_array_header_t *header = raxel_get_array_header(array);
+    raxel_free(header->__allocator, (void *)((char *)array - sizeof(__raxel_array_header_t)));
 }
 
-// iterators are implemented wrong rn
-
 static void *__raxel_array_it_next(raxel_iterator_t *it) {
-    __raxel_array_header_t *header = (__raxel_array_header_t *)((raxel_size_t *)it->__data - sizeof(__raxel_array_header_t));
-    return (void *)((raxel_size_t *)it->__data + header->__stride);
+    __raxel_array_header_t *header = raxel_get_array_header(it->__data);
+    return (void *)((char *)it->__data + header->__stride);
 }
 
 static void *__raxel_array_it_current(raxel_iterator_t *it) {
@@ -44,53 +54,58 @@ raxel_iterator_t raxel_array_iterator(void *array) {
 }
 
 /**------------------------------------------------------------------------
- *                           RAXEL_LIST (continuous memory)
+ *                           RAXEL LIST (continuous memory)
  *------------------------------------------------------------------------**/
 
 void *__raxel_list_create(raxel_allocator_t *allocator, raxel_size_t size, raxel_size_t stride) {
     raxel_size_t header_size = sizeof(__raxel_list_header_t);
     raxel_size_t data_size = size * stride;
-    void *list = raxel_malloc(allocator, header_size + data_size);
-    __raxel_list_header_t *header = list;
-    *header = (__raxel_list_header_t){
-        .__size = size,
-        .__stride = stride,
-        .__capacity = size,
-        .__allocator = allocator};
-    // return the pointer to the data
-    return (void *)((raxel_size_t *)list + header_size);
+    void *block = raxel_malloc(allocator, header_size + data_size);
+    __raxel_list_header_t *header = (__raxel_list_header_t *)block;
+    header->__size = size;
+    header->__stride = stride;
+    header->__capacity = size;
+    header->__allocator = allocator;
+    // Return pointer to data (right after header)
+    return (void *)((char *)block + header_size);
 }
 
 void __raxel_list_destroy(void *list) {
-    __raxel_list_header_t *header = (__raxel_list_header_t *)((raxel_size_t *)list - sizeof(__raxel_list_header_t));
-    raxel_free(header->__allocator, (void *)((raxel_size_t *)list - sizeof(__raxel_list_header_t)));
+    if (!list) return;
+    __raxel_list_header_t *header = raxel_get_list_header(list);
+    raxel_free(header->__allocator, (void *)((char *)list - sizeof(__raxel_list_header_t)));
 }
 
-void __raxel_list_resize(void **list_ptr, raxel_size_t size) {
-    __raxel_list_header_t *header = (__raxel_list_header_t *)((raxel_size_t *)*list_ptr - sizeof(__raxel_list_header_t));
+void __raxel_list_resize(void **list_ptr, raxel_size_t new_capacity) {
+    if (!list_ptr || !(*list_ptr)) return;
+    __raxel_list_header_t *old_header = raxel_get_list_header(*list_ptr);
     raxel_size_t header_size = sizeof(__raxel_list_header_t);
-    raxel_size_t data_size = size * header->__stride;
-    void *new_list = raxel_malloc(header->__allocator, header_size + data_size);
-    memcpy(new_list, *list_ptr, header->__size * header->__stride);
-    raxel_free(header->__allocator, (void *)((raxel_size_t *)*list_ptr - sizeof(__raxel_list_header_t)));
-    *list_ptr = (void *)((raxel_size_t *)new_list + header_size);
+    raxel_size_t data_size = new_capacity * old_header->__stride;
+    void *new_block = raxel_malloc(old_header->__allocator, header_size + data_size);
+    __raxel_list_header_t *new_header = (__raxel_list_header_t *)new_block;
+    /* Copy header information from old block */
+    memcpy(new_header, old_header, header_size);
+    /* Copy data from old block into new block */
+    memcpy((char *)new_block + header_size, *list_ptr, old_header->__size * old_header->__stride);
+    new_header->__capacity = new_capacity;  // update capacity field
+    raxel_free(old_header->__allocator, (void *)((char *)*list_ptr - header_size));
+    *list_ptr = (void *)((char *)new_block + header_size);
 }
 
 void __raxel_list_push_back(void **list_ptr, void *data) {
-    __raxel_list_header_t *header = (__raxel_list_header_t *)((raxel_size_t *)*list_ptr - sizeof(__raxel_list_header_t));
+    if (!list_ptr || !(*list_ptr)) return;
+    __raxel_list_header_t *header = raxel_get_list_header(*list_ptr);
     if (header->__size == header->__capacity) {
         __raxel_list_resize(list_ptr, header->__capacity * 2);
-        header->__capacity *= 2;
+        header = raxel_get_list_header(*list_ptr);  // update header after resize
     }
-    memcpy((void *)((raxel_size_t *)*list_ptr + header->__size * header->__stride), data, header->__stride);
+    memcpy((char *)*list_ptr + header->__size * header->__stride, data, header->__stride);
     header->__size++;
 }
 
-// iterators are implemented wrong rn
-
 static void *__raxel_list_it_next(raxel_iterator_t *it) {
-    __raxel_list_header_t *header = (__raxel_list_header_t *)((raxel_size_t *)it->__data - sizeof(__raxel_list_header_t));
-    return (void *)((raxel_size_t *)it->__data + header->__stride);
+    __raxel_list_header_t *header = raxel_get_list_header(it->__data);
+    return (void *)((char *)it->__data + header->__stride);
 }
 
 static void *__raxel_list_it_current(raxel_iterator_t *it) {
@@ -113,19 +128,19 @@ static void __raxel_string_reserve(raxel_string_t *string, raxel_size_t new_capa
         // Already have enough capacity
         return;
     }
-    // Allocate new buffer
+    // Allocate new buffer (plus one for the null terminator)
     char *new_data = (char *)raxel_malloc(string->__allocator, new_capacity + 1);
-    // Copy old data
+    // Copy old data if any
     if (string->__data && string->__size > 0) {
         memcpy(new_data, string->__data, string->__size);
     }
     // Null-terminate
     new_data[string->__size] = '\0';
-    // Free old buffer
+    // Free old buffer if allocated
     if (string->__data) {
         raxel_free(string->__allocator, string->__data);
     }
-    // Update string
+    // Update string struct
     string->__data = new_data;
     string->__capacity = new_capacity;
 }
@@ -136,8 +151,7 @@ raxel_string_t raxel_string_create(raxel_allocator_t *allocator, raxel_size_t ca
     string.__size = 0;
     string.__capacity = 0;
     string.__data = NULL;
-
-    // Reserve initial capacity (plus 1 for the null terminator)
+    // Reserve initial capacity
     __raxel_string_reserve(&string, capacity);
     if (string.__data) {
         string.__data[0] = '\0';
@@ -158,35 +172,25 @@ void raxel_string_destroy(raxel_string_t *string) {
 
 void raxel_string_resize(raxel_string_t *string, raxel_size_t size) {
     if (!string) return;
-
-    // If we need more capacity, reserve
     if (size > string->__capacity) {
         __raxel_string_reserve(string, size);
     }
-
-    // If shrinking, just adjust size
     string->__size = size;
-    // Always null-terminate
     string->__data[size] = '\0';
 }
 
 void raxel_string_push_back(raxel_string_t *string, char c) {
     if (!string) return;
-
-    // Expand if needed
     if (string->__size == string->__capacity) {
         raxel_size_t new_cap = (string->__capacity == 0) ? 8 : (string->__capacity * 2);
         __raxel_string_reserve(string, new_cap);
     }
-
     string->__data[string->__size++] = c;
     string->__data[string->__size] = '\0';
 }
 
 void raxel_string_append_n(raxel_string_t *string, const char *str, raxel_size_t n) {
     if (!string || !str || n == 0) return;
-
-    // Ensure enough capacity
     if (string->__size + n > string->__capacity) {
         raxel_size_t new_cap = string->__capacity;
         while (string->__size + n > new_cap) {
@@ -194,10 +198,8 @@ void raxel_string_append_n(raxel_string_t *string, const char *str, raxel_size_t
         }
         __raxel_string_reserve(string, new_cap);
     }
-
     memcpy(string->__data + string->__size, str, n);
     string->__size += n;
-    // Null-terminate
     string->__data[string->__size] = '\0';
 }
 
@@ -214,7 +216,6 @@ char *raxel_string_data(raxel_string_t *string) {
 
 char *raxel_string_to_cstr(raxel_string_t *string) {
     if (!string) return NULL;
-    // Ensure null termination
     if (string->__data && string->__size <= string->__capacity) {
         string->__data[string->__size] = '\0';
     }
@@ -243,44 +244,34 @@ raxel_array(raxel_string_t) raxel_string_split(raxel_string_t *string, char deli
         return raxel_array_create(raxel_string_t, string ? string->__allocator : NULL, 0);
     }
 
-    // First pass: count how many delimiters (which determines array size)
+    // First pass: count delimiters to determine number of substrings
     raxel_size_t delim_count = 0;
     for (raxel_size_t i = 0; i < string->__size; i++) {
         if (string->__data[i] == delim) {
             delim_count++;
         }
     }
-    // We will have delim_count + 1 substrings
     raxel_size_t substring_count = delim_count + 1;
+    raxel_array(raxel_string_t) result = raxel_array_create(raxel_string_t, string->__allocator, substring_count);
 
-    // Allocate array of raxel_string_t
-    raxel_array(raxel_string_t) result =
-        raxel_array_create(raxel_string_t, string->__allocator, substring_count);
-
-    // Second pass: split
+    // Second pass: split into substrings
     raxel_size_t start_idx = 0;
     raxel_size_t result_idx = 0;
     for (raxel_size_t i = 0; i < string->__size; i++) {
         if (string->__data[i] == delim) {
-            // Create substring from [start_idx..i)
             raxel_size_t length = i - start_idx;
             raxel_string_t s = raxel_string_create(string->__allocator, length);
             raxel_string_append_n(&s, &string->__data[start_idx], length);
-
-            // Store in array
             result[result_idx++] = s;
-            // Move start past delimiter
             start_idx = i + 1;
         }
     }
-
-    // Add the final substring (from start_idx to end)
+    // Final substring (from last delimiter to end)
     {
         raxel_size_t length = string->__size - start_idx;
         raxel_string_t s = raxel_string_create(string->__allocator, length);
         raxel_string_append_n(&s, &string->__data[start_idx], length);
         result[result_idx++] = s;
     }
-
     return result;
 }
