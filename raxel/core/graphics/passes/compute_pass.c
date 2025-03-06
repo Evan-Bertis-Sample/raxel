e41f2a07841bf0854b86a225521c9f44e4672558
evan-bertis-sample <esample21@gmail.com>
2025-03-06 08:49:23 -0600

    got the application running again, nothing presenting to the sscreen, commet out push constant stuff

--- raxel/core/graphics/passes/compute_pass.c
+++ raxel/core/graphics/passes/compute_pass.c
@@ -80,8 +80,8 @@ raxel_compute_shader_t *raxel_compute_shader_create(raxel_pipeline_t *pipeline, 
     raxel_list_push_back(shader->__push_layout, entry_fov);
     
     shader->push_data_size = push_data_size;
-    shader->push_data = malloc(push_data_size);
-    memset(shader->push_data, 0, push_data_size);
+    // shader->push_data = malloc(push_data_size);
+    // memset(shader->push_data, 0, push_data_size);
     shader->descriptor_set = VK_NULL_HANDLE;
     return shader;
 }
@@ -104,7 +104,7 @@ void raxel_compute_shader_push_constant(raxel_compute_shader_t *shader, const ch
             return;
         }
     }
-    fprintf(stderr, "Push constant '%s' not found!\n", name);
+    RAXEL_CORE_LOG_ERROR("Push constant not found: %s\n", name);
 }
 
 // -----------------------------------------------------------------------------
@@ -133,9 +133,9 @@ static void compute_pass_on_begin(raxel_pipeline_pass_t *pass, raxel_pipeline_t 
         vkCmdBindDescriptorSets(cmd_buf, VK_PIPELINE_BIND_POINT_COMPUTE, ctx->compute_shader->pipeline_layout,
                                 0, 1, &ctx->compute_shader->descriptor_set, 0, NULL);
     }
-    
-    vkCmdPushConstants(cmd_buf, ctx->compute_shader->pipeline_layout, VK_SHADER_STAGE_COMPUTE_BIT,
-                       0, ctx->compute_shader->push_data_size, ctx->compute_shader->push_data);
+
+    // vkCmdPushConstants(cmd_buf, ctx->compute_shader->pipeline_layout, VK_SHADER_STAGE_COMPUTE_BIT,
+    //                    0, ctx->compute_shader->push_data_size, ctx->compute_shader->push_data);
     
     vkCmdDispatch(cmd_buf, ctx->dispatch_x, ctx->dispatch_y, ctx->dispatch_z);
     
@@ -162,19 +162,15 @@ static void compute_pass_on_end(raxel_pipeline_pass_t *pass, raxel_pipeline_t *p
     }
 }
 
-// Default blit callback: Copy the computed output into the pipeline target.
-// This function records a command buffer to blit from the compute shader's output
-// (if set in the context; otherwise, from the internal target indicated by blit_target)
-// to the pipeline target image, then calls raxel_pipeline_present().
 void raxel_compute_pass_blit(raxel_compute_pass_context_t *context, raxel_pipeline_t *pipeline) {
     VkDevice device = pipeline->resources.device;
-    // Determine the source image.
-    VkImage src_image = (context->output_image != VK_NULL_HANDLE) ? context->output_image
-                        : pipeline->targets.internal[context->blit_target].image;
-    
-    // Destination: the pipeline target (we assume it matches the swapchain extent).
+    // Determine source image: use context->output_image if provided; otherwise, use the internal target.
+    VkImage src_image = (context->output_image != VK_NULL_HANDLE) ?
+                        context->output_image :
+                        pipeline->targets.internal[context->blit_target].image;
+    // The destination is the pipeline target image.
     VkImage dst_image = pipeline->targets.internal[context->blit_target].image;
-    
+
     // Allocate a temporary command buffer from the graphics command pool.
     VkCommandBufferAllocateInfo alloc_info = {0};
     alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -183,13 +179,15 @@ void raxel_compute_pass_blit(raxel_compute_pass_context_t *context, raxel_pipeli
     alloc_info.commandBufferCount = 1;
     VkCommandBuffer cmd_buf;
     VK_CHECK(vkAllocateCommandBuffers(device, &alloc_info, &cmd_buf));
-    
+
+    // Begin recording the command buffer.
     VkCommandBufferBeginInfo begin_info = {0};
     begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
     begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
     VK_CHECK(vkBeginCommandBuffer(cmd_buf, &begin_info));
-    
-    // Set up a blit from src_image to dst_image.
+
+    // For simplicity, assume both images are already in the appropriate transfer layouts.
+    // Here we blit the entire image.
     VkExtent2D extent = pipeline->resources.swapchain.extent;
     VkImageBlit blit = {0};
     blit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
@@ -198,24 +196,31 @@ void raxel_compute_pass_blit(raxel_compute_pass_context_t *context, raxel_pipeli
     blit.srcSubresource.layerCount = 1;
     blit.srcOffsets[0] = (VkOffset3D){0, 0, 0};
     blit.srcOffsets[1] = (VkOffset3D){(int32_t)extent.width, (int32_t)extent.height, 1};
-    blit.dstSubresource = blit.srcSubresource;
+
+    blit.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    blit.dstSubresource.mipLevel = 0;
+    blit.dstSubresource.baseArrayLayer = 0;
+    blit.dstSubresource.layerCount = 1;
     blit.dstOffsets[0] = (VkOffset3D){0, 0, 0};
     blit.dstOffsets[1] = (VkOffset3D){(int32_t)extent.width, (int32_t)extent.height, 1};
-    
-    // For simplicity, assume images are already in the correct layouts.
+
     vkCmdBlitImage(cmd_buf, src_image, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
                    dst_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
                    1, &blit, VK_FILTER_LINEAR);
-    
+
     VK_CHECK(vkEndCommandBuffer(cmd_buf));
-    
+
     VkSubmitInfo submit_info = {0};
     submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
     submit_info.commandBufferCount = 1;
     submit_info.pCommandBuffers = &cmd_buf;
+    // We assume synchronization (and proper layout transitions) is handled elsewhere.
     VK_CHECK(vkQueueSubmit(pipeline->resources.queue_graphics, 1, &submit_info, VK_NULL_HANDLE));
     vkQueueWaitIdle(pipeline->resources.queue_graphics);
     vkFreeCommandBuffers(device, pipeline->resources.cmd_pool_graphics, 1, &cmd_buf);
+
+    // Do not call vkQueuePresentKHR here.
+    // The pipeline's present() function will use the target image to present on screen.
 }
 
 raxel_pipeline_pass_t raxel_compute_pass_create(raxel_compute_pass_context_t *context) {
